59b0d4cc146c79adcc6e11d4238f3d0d18da3b24
Rudolfs <ruudisrudolfs@gmail.com>
2025-06-26 23:13:47 +0300

    did some work

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/NavBar";
 import Homepage from "./Pages/Homepage.jsx";
-import ProductsSection from "./Pages/ProductsSection.jsx"; // Make sure to import your ProductsSection
-import ProductDetailPage from "./Pages/ProductDetailPage.jsx"; // Import the new ProductDetailPage
+import ProductsSection from "./Pages/ProductsSection.jsx";
+import ProductDetailPage from "./Pages/ProductDetailPage.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Gallery from "./Pages/Gallery.jsx";
 import About from "./Pages/About.jsx";
@@ -12,7 +12,7 @@ import Contact from "./Pages/Contact.jsx";
 function App() {
     return (
         <ParallaxProvider>
-            <Router>
+            <Router basename="/">
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Homepage />} />
@@ -24,6 +24,8 @@ function App() {
                     <Route path="/galleri" element={<Gallery />} />
                     <Route path="/om-oss" element={<About />} />
                     <Route path="/kontakt" element={<Contact />} />
+                    {/* Add a catch-all route for 404s */}
+                    <Route path="*" element={<Homepage />} />
                 </Routes>
                 <Footer />
             </Router>
