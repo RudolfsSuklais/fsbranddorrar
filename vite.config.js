58e5e403731dc64221255da873f650eb0545e418
Rudolfs <ruudisrudolfs@gmail.com>
2025-06-26 23:20:48 +0300

    did some work

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/", // This is crucial for Render.com
-    build: {
-        outDir: "dist",
-        emptyOutDir: true,
-    },
 });
